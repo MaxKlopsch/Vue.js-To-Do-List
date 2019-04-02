98190fad07cd5ce5184aea43fc875cc136b80ae0
Max Klopsch <32960120+MaxKlopsch@users.noreply.github.com>
2019-04-02 12:33:07 +0100

    Add Todos component and output title to screen

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,37 @@
 <template>
   <div id="app">
-
+    <Todos v-bind:todos="todos" />
   </div>
 </template>
 
 <script>
+import Todos from './components/Todos';
+
 export default {
   name: 'app',
   components: {
-
+    Todos
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: 'Get milk',
+          completed: false
+        },
+        {
+          id: 2,
+          title: 'Do the laundry',
+          completed: true
+        },
+        {
+          id: 3,
+          title: 'Vacuum the bedroom',
+          completed: false
+        }
+      ]
+    }
   }
 }
 </script>
